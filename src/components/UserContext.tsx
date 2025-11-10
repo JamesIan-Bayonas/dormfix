5f9bfa4bb246e49dfffe95b2708ae39b1a147e9e
JamesIan-Bayonas <jamesianbayonas21@gmail.com>
2025-11-10 21:52:05 +0800

    Type safety configurations

--- src/components/UserContext.tsx
+++ src/components/UserContext.tsx
@@ -1,19 +1,17 @@
 // UserContext.tsx
 import React, { createContext, useState, useContext, type ReactNode } from 'react'; // import ReactNode type
-import type { User, UserRole, AuthContextType } from '../types/types.ts'; //type is a reserved word in TS
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+import type { User, UserRole, AuthContextType } from '../types/types.ts'; // type imports
+const AuthContext = createContext<undefined | AuthContextType>(undefined);
 
-// Mock user data for the prototype (replace with API calls in a real application)
-const MOCK_USERS : Record<string, User>= {
+const MOCK_USERS : Record<string , User>= {
   'landlord@dormfix.com': { id: 'L1', name: 'Admin Dela Cruz', role: 'landlord', email: 'landlord@dormfix.com' },
   'tenant@dormfix.com': { id: 'T101', name: 'Maria Santos', role: 'tenant', email: 'tenant@dormfix.com' },
 };
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>(null); // useState [value, updateFunction]
 
   const login = (email: string, role: UserRole) => {
-    // In a real app, you would verify password here and fetch user data from a server.
     const mockUser = MOCK_USERS[email as keyof typeof MOCK_USERS];
     
     if (mockUser && mockUser.role === role) {
